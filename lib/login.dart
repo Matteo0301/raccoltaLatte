407c43e7293ec63c7ecde585bc36640f4b56afdb
Matteo0301 <matteomariotti0301@gmail.com>
2025-02-02 15:14:56 +0100

    Added google login

--- lib/login.dart
+++ lib/login.dart
@@ -18,6 +18,21 @@ class LoginState extends State<Login> {
   TextEditingController userController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
+  Future<UserCredential> signInWithGoogle() async {
+    // Create a new provider
+    GoogleAuthProvider googleProvider = GoogleAuthProvider();
+
+    googleProvider
+        .addScope('https://www.googleapis.com/auth/contacts.readonly');
+    googleProvider.setCustomParameters({'login_hint': 'user@example.com'});
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithPopup(googleProvider);
+
+    // Or use signInWithRedirect
+    // return await FirebaseAuth.instance.signInWithRedirect(googleProvider);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,58 +74,111 @@ class LoginState extends State<Login> {
                   padding:
                       const EdgeInsets.symmetric(horizontal: 8, vertical: 16.0),
                   child: Center(
-                    child: ElevatedButton(
-                      onPressed: () async {
-                        String username = userController.text;
-                        String password = passwordController.text;
+                    child: Column(children: [
+                      ElevatedButton(
+                        onPressed: () async {
+                          String username = userController.text;
+                          String password = passwordController.text;
 
-                        try {
-                          await login(username, password);
-                          final LoggedUser? user = await getUserData();
-                          FirebaseAnalytics.instance.logLogin();
+                          try {
+                            await login(username, password);
+                            final LoggedUser? user = await getUserData();
 
-                          if (user != null && context.mounted) {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => Home(
-                                        title: widget.title,
-                                        username: user.username,
-                                        admin: user.admin,
-                                      )),
-                            );
+                            if (user != null && context.mounted) {
+                              FirebaseAnalytics.instance.logLogin();
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Home(
+                                          title: widget.title,
+                                          username: user.username,
+                                          admin: user.admin,
+                                        )),
+                              );
+                            }
+                          } on FirebaseAuthException catch (e) {
+                            String errorMessage =
+                                "Impossibile effettuare il login";
+                            switch (e.code) {
+                              case "invalid-email":
+                                errorMessage = "Email non valida";
+                                break;
+                              case "user-disabled":
+                                errorMessage = "Utente disabilitato";
+                                break;
+                              case "user-not-found":
+                                errorMessage = "Utente non trovato";
+                                break;
+                              case "wrong-password":
+                                errorMessage = "Password errata";
+                                break;
+                              case "too-many-requests":
+                                errorMessage = "Troppi login";
+                                break;
+                              case "network-request-failed":
+                                errorMessage = "Impossibile connettersi";
+                                break;
+                            }
+                            logAndShow(errorMessage);
                           }
-                        } on FirebaseAuthException catch (e) {
-                          String errorMessage =
-                              "Impossibile effettuare il login";
-                          switch (e.code) {
-                            case "invalid-email":
-                              errorMessage = "Email non valida";
-                              break;
-                            case "user-disabled":
-                              errorMessage = "Utente disabilitato";
-                              break;
-                            case "user-not-found":
-                              errorMessage = "Utente non trovato";
-                              break;
-                            case "wrong-password":
-                              errorMessage = "Password errata";
-                              break;
-                            case "too-many-requests":
-                              errorMessage = "Troppi login";
-                              break;
-                            case "network-request-failed":
-                              errorMessage = "Impossibile connettersi";
-                              break;
-                          }
-                          logAndShow(errorMessage);
-                        }
-                      },
-                      child: const Padding(
-                        padding: EdgeInsets.all(10.0),
-                        child: Text('Login', style: TextStyle(fontSize: 20)),
+                        },
+                        child: const Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: Text('Login', style: TextStyle(fontSize: 20)),
+                        ),
                       ),
-                    ),
+                      ElevatedButton(
+                        onPressed: () async {
+                          try {
+                            await signInWithGoogle();
+
+                            final LoggedUser? user = await getUserData();
+
+                            if (user != null && context.mounted) {
+                              FirebaseAnalytics.instance.logLogin();
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Home(
+                                          title: widget.title,
+                                          username: user.username,
+                                          admin: user.admin,
+                                        )),
+                              );
+                            }
+                          } on FirebaseAuthException catch (e) {
+                            String errorMessage =
+                                "Impossibile effettuare il login";
+                            switch (e.code) {
+                              case "invalid-email":
+                                errorMessage = "Email non valida";
+                                break;
+                              case "user-disabled":
+                                errorMessage = "Utente disabilitato";
+                                break;
+                              case "user-not-found":
+                                errorMessage = "Utente non trovato";
+                                break;
+                              case "wrong-password":
+                                errorMessage = "Password errata";
+                                break;
+                              case "too-many-requests":
+                                errorMessage = "Troppi login";
+                                break;
+                              case "network-request-failed":
+                                errorMessage = "Impossibile connettersi";
+                                break;
+                            }
+                            logAndShow(errorMessage);
+                          }
+                        },
+                        child: const Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: Text('Login with Google',
+                              style: TextStyle(fontSize: 20)),
+                        ),
+                      ),
+                    ]),
                   ),
                 ),
               ],
